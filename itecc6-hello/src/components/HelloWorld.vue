2ded91ed9d4d8ce1cabf3664bf861d52b3617ff8
Joshua Casem <jazecasem@gmail.com>
2026-04-16 12:45:15 +0800

    FIrst commit ko to!

--- itecc6-hello/src/components/HelloWorld.vue
+++ itecc6-hello/src/components/HelloWorld.vue
@@ -12,5 +12,6 @@ const count = ref(0)
     <h1>HELLO I'M BULLONG</h1>
     <h2>WELCOME TO MY VUEJS PROJECT</h2>
     <img :src="heroImg" alt="Hero Image" class="hero-image" height="300" width="300">  
+    <h2>HAWAK MO ANG BRIP</h2>
   </section>
 </template>
